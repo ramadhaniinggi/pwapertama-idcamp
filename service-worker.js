a3b5e71fdaedc532313e66fd3a659c58ff68a30c
Inggi Ramadhani <ramadhaniinggi@gmail.com>
2020-06-24 14:08:07 +0800

    menambahkan berbagai ukuran icon di manifest.json

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const cache_name = "firsrpwa";
+const cache_name = "pwacache";
 // asset utk ditaruh di cache storage
 let urlsToCache = [
   "/",
@@ -20,6 +20,15 @@ let urlsToCache = [
   "/img/womenclothes_4.jpg",
   "/css/style.css",
   "/css/flUhRq6tzZclQEJ-Vdg-IuiaDsNc.woff2",
+  "/manifest.json",
+  "/img/icon-128x128.png",
+  "/img/icon-144x144.png",
+  "/img/icon-152x152.png",
+  "/img/icon-192x192.png",
+  "/img/icon-384x384.png",
+  "/img/icon-512x512.png",
+  "/img/icon-72x72.png",
+  "/img/icon-96x96.png",
 ];
 
 // utk menyimpan/menambahkan cache di cache storage
